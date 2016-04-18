8e37ae60df10426dcf1913e6684739538b5c8eaa
HuffyTomson <thomaswhuffman@gmail.com>
2016-04-18 17:54:39 -0500

    added tools

---
+++ PhotoBooth/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Huffy.Utilities;
+
+public class GameManager : MonoBehaviour
+{
+    public int id = 0;
+    private StateMachine<GameManager> sm;
+    
+    IEnumerator Start ()
+    {
+        VersionNumber.Initialize();
+        Config.Initialize();
+
+        yield return new WaitForSeconds(0.125f);
+        Container.Load("Container").PrintString();
+
+        sm = new StateMachine<GameManager>(new TestStateOne(this));
+
+        yield return new WaitForSeconds(0.5f);
+        TwilioManager.SendSMS();
+    }
+
+    void Update()
+    {
+        if (sm != null)
+            sm.Update();
+    }
+    
+}
---
+++ PhotoBooth/Assets/Scripts/Managers/TwilioManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Huffy.Utilities;
+
+public class TwilioManager : SingletonBehaviour<TwilioManager>
+{
+    string url = "api.twilio.com/2010-04-01/Accounts/";
+    string service = "/Messages.json";
+    public string from;
+    public string to;
+    public string account_sid;
+    public string auth;
+    public string body;
+
+    public static void SendSMS()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("To", Instance.to);
+        form.AddField("From", Instance.from);
+        //string bodyWithoutSpace = body.Replace (" ", "%20");//Twilio doesn't need this conversion
+        form.AddField("Body", Instance.body);
+        string completeurl = "https://" + Instance.account_sid + ":" + Instance.auth + "@" + Instance.url + Instance.account_sid + Instance.service;
+        Debug.Log(completeurl);
+        WWW www = new WWW(completeurl, form);
+        Instance.StartCoroutine(Instance.WaitForRequest(www));
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok! SMS sent through Web API: " + www.text);
+        }
+        else {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+}
